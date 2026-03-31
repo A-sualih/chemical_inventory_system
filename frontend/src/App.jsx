5caf4eb02fc03683ce6adcc4c2206afdf78d5b97
Ahmed Sualih <sualihahmed26@gmail.com>
2026-03-31 11:11:41 +0300

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      
+      <div className="bg-white p-8 rounded-xl shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-blue-600 mb-4">
+          Tailwind CSS is Working 🚀
+        </h1>
+
+        <p className="text-gray-600 mb-6">
+          If you see this styled box, Tailwind is installed correctly.
+        </p>
+
+        <button className="bg-green-50 hover:bg-green-600 text-white px-6 py-2 rounded-lg">
+          Test Button
+        </button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Chemicals.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chemicals = () => {
+  return (
+    <div>
+      <h2>Chemicals page</h2>
+    </div>
+  )
+}
+
+export default Chemicals
