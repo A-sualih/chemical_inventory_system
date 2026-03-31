10e33e6110a32853772bb578295973f39a62e43f
Ahmed Sualih <sualihahmed26@gmail.com>
2026-03-31 11:32:30 +0300

    first render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,13 @@ function App() {
         <h1 className="text-3xl font-bold text-blue-600 mb-4">
           Tailwind CSS is Working 🚀
         </h1>
-
         <p className="text-gray-600 mb-6">
           If you see this styled box, Tailwind is installed correctly.
         </p>
-
         <button className="bg-green-50 hover:bg-green-600 text-white px-6 py-2 rounded-lg">
           Test Button
         </button>
       </div>
-
     </div>
   );
 }
