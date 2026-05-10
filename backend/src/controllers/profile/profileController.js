f7b947e42f959427704371bd0f7be633ca4b3a36
Amir Mesfin <amir.mesfin136@gmail.com>
2026-05-10 11:55:59 -0400

    feat: implement audit logging for sensitive profile updates in profileController

--- backend/src/controllers/profile/profileController.js
+++ backend/src/controllers/profile/profileController.js
@@ -1,4 +1,5 @@
 const User = require('../../models/User');
+const AuditLog = require('../../models/AuditLog');
 
 exports.getProfile = async (req, res) => {
   try {
@@ -23,15 +24,50 @@ exports.updateProfile = async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
+    const trackedChanges = {};
+
     // Update allowable fields
-    if (name !== undefined) user.name = name;
+    if (name !== undefined) {
+      if (user.name !== name) trackedChanges.name = { old: user.name, new: name };
+      user.name = name;
+    }
     if (phone !== undefined) user.phone = phone;
-    if (profile_photo !== undefined) user.profile_photo = profile_photo;
+    if (profile_photo !== undefined) {
+      if (user.profile_photo !== profile_photo) trackedChanges.profile_photo = { old: user.profile_photo, new: profile_photo };
+      user.profile_photo = profile_photo;
+    }
     if (email_preferences !== undefined) user.email_preferences = email_preferences;
     if (mfa_enabled !== undefined) user.mfa_enabled = mfa_enabled;
 
     await user.save();
     
+    // Log sensitive profile state transitions for the auditor
+    if (Object.keys(trackedChanges).length > 0) {
+      await AuditLog.create({
+        user: {
+          id: user._id,
+          name: user.name,
+          role: user.role,
+          email: user.email
+        },
+        action: 'UPDATE',
+        target: {
+          type: 'user',
+          id: user._id.toString(),
+          name: user.name
+        },
+        details: 'User updated sensitive profile properties',
+        changes: {
+          oldValue: Object.keys(trackedChanges).reduce((acc, key) => ({ ...acc, [key]: trackedChanges[key].old }), {}),
+          newValue: Object.keys(trackedChanges).reduce((acc, key) => ({ ...acc, [key]: trackedChanges[key].new }), {})
+        },
+        metadata: {
+          ip: req.ip,
+          userAgent: req.headers['user-agent']
+        }
+      });
+    }
+
     res.json({ message: 'Profile updated successfully', user: {
       id: user._id,
       name: user.name,
